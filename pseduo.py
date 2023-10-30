110057c127980af3cad81c7e1c6d724b24c95722
FlashRevix <chkoz31@gmail.com>
2023-10-30 13:03:40 -0400

    Co-authored-by: SarahSchafer12 <SarahSchafer12@users.noreply.github.com>

---
+++ pseduo.py
@@ -0,0 +1,11 @@
+"""
+PSEUDOCODE 1: text --> outline
+
+Step 1) import dictonary as data set
+Step 2) create function to print hangman board, randomly pick word from dictonary
+Step 3) take guesses from player 
+Step 4) compute their input, print if right or hang if wrong, let them know if they win or lose
+
+PSEUDOCODE 2: flow chart with symbols 
+
+"""
